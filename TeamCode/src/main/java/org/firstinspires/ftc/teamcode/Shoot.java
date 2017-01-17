709225c3d2f826ec0f28bc9660735df4cef3c841
JJB20 <jj.twenty20@gmail.com>
2017-01-16 19:55:30 -0500

    testing change on new branch

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Shoot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Shoot.java
@@ -5,7 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 /**
- * Created by Troy on 10/01/16.
+ *
   This works
 
  */
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TwoShootCenterBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TwoShootCenterBlue.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
-//jus
+//Justin > Mitch
 /**
 
   This works
